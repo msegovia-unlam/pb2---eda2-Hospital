ec1f917cd4edd67e3d8f5840dd74be9bc38bdcc0
sacorv <scortesvaldes@alumno.unlam.edu.ar>
2020-11-10 19:58:00 -0300

    agrego test queSePuedaCancelarUnTurno

--- test/ar/edu/unlam/pb2/hospital/HospitalTest.java
+++ test/ar/edu/unlam/pb2/hospital/HospitalTest.java
@@ -162,6 +162,25 @@ public class HospitalTest {
 		assertNotSame(especialidadAEncontrar, especialidad);
 	}
 	
+	@Test
+	public void queSePuedaCancelarUnTurno() {
+		SistemaHospital hospital = new SistemaHospital("Centro San Justo");
+		Especialidad kinesiologia = new Especialidad(2, "Kinesiologia");
+		hospital.agregarEspecialidad(kinesiologia);
+		Medico medico = new Medico("Maria", "Gimenez", 25666222, 10, "02-07-2000", 50000.0, 21222, "Kinesiologia");
+		hospital.registrarMedico(medico);
+		hospital.registrarMedicoEnEspecialidad(10, 2);
+		Administrativo admin = new Administrativo("Gustavo", "Ruiz", 30299991, 3, "10-03-2019", 45.000, "Recepcionista");
+		hospital.registrarAdministrativo(admin);
+		Paciente paciente1 = new Paciente("Luis", "Gomez", 29123555, 5, "Hipertenso", 70.5, 1.75);
+		hospital.registrarPaciente(paciente1);
+		hospital.asignarTurnoAPaciente(5, 2, 10, 3, "6-11-2020");
+		hospital.cancelarTurno(1);
+		Integer cantidadDeTurnos = hospital.getTurnosAsignados().size();
+		Integer cantidadEsperada = 0;
+		
+		assertEquals(cantidadEsperada, cantidadDeTurnos);
+	}
 	
 	
 }
