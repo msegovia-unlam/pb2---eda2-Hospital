2744e173f7e4e15e042102986f105752123f4a2d
msegovia-unlam <71356046+msegovia-unlam@users.noreply.github.com>
2020-11-10 20:11:28 -0300

    agrego testQuePermitaRegistrarPisos y testQueRegistreUnMedicoEnUnaEspecialidad

--- test/ar/edu/unlam/pb2/hospital/HospitalTest.java
+++ test/ar/edu/unlam/pb2/hospital/HospitalTest.java
@@ -202,4 +202,26 @@ public class HospitalTest {
 		assertEquals(cantidadEsperada, cantidadDeConsultasSinturno);
 	}
 	
+	@Test
+	public void testQuePermitaRegistrarPisos(){
+		Piso piso1 = new Piso(1, 1);		
+		
+		SistemaHospital hospital1 = new SistemaHospital("nuevoHospital");		
+		
+		assertTrue(hospital1.registrarPiso(piso1));
+	}
+	
+	
+	@Test
+	public void testQueRegistreUnMedicoEnUnaEspecialidad(){
+		SistemaHospital hospital1 = new SistemaHospital("nuevoHospital");
+		Medico medico1 = new Medico("Martin", "Segovia", 32830303, 20, "1deMarzo", 50000.0, 456123, "Cirujano");
+		Especialidad especialidad1 = new Especialidad(11, "Cirugia");
+		
+		hospital1.registrarMedico(medico1);
+		hospital1.agregarEspecialidad(especialidad1);		
+		
+		assertTrue(hospital1.registrarMedicoEnEspecialidad(20, 11));
+	}
+	
 }
